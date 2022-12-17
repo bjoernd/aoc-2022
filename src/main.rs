4a385a88612d4eb432a7c6cd3fbe5aecd94e59f5
Bjoern Doebel <bjoern.doebel@gmail.com>
2022-12-17 09:55:36 +0100

    wip day11

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,8 @@ mod day9;
 use day9::Day9;
 mod day10;
 use day10::Day10;
+mod day11;
+use day11::Day11;
 
 /// Reads the lines from the input file into a relevant
 /// model of the data for the day's solution.
@@ -61,6 +63,7 @@ fn get_day_solution(day: usize, lines: impl Iterator<Item = String>) -> Box<dyn 
         8 => Box::new(Day8::from_lines(lines)),
         9 => Box::new(Day9::from_lines(lines)),
         10 => Box::new(Day10::from_lines(lines)),
+        11 => Box::new(Day11::from_lines(lines)),
         _other => panic!("Day hasn't been solved yet"),
     }
 }
